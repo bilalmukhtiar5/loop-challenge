b58e9b4b99a767382665e042b4fee8bc2ef1999b
bilalmukhtiar5 <billuedwardian08@gmail.com>
2023-09-20 13:08:20 +0500

    task4 done

---
+++ script.js
@@ -0,0 +1,44 @@
+/*
+
+Task 1
+
+for(let i=1;i<=10;i++){
+console.log(i);
+}
+
+*/
+
+/*
+Task 2 
+
+for (let i=0; i<=20; i=i+2){
+console.log(i);
+}
+
+
+*/
+
+/*
+
+Task 3
+
+Table task
+ for (let start=1;start<=10;start=start+1){
+ console.log(`5*${start} = ${2*start}`)
+ }
+
+*/
+ 
+/*
+Task 4
+
+let firstNumber =0;
+ let secondNumber = 1;
+ 
+ for(let start=0; start<10; start++){
+  let result = firstNumber + secondNumber;
+  firstNumber = secondNumber;
+  secondNumber = result;
+  console.log(result)
+ }
+*/
